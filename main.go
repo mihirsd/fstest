4b60e09ed601e008c910d40b709e07f0e05884cd
mihirsd <35537937+mihirsd@users.noreply.github.com>
2018-07-25 20:15:10 +0530

    Remove log handler

--- main.go
+++ main.go
@@ -6,21 +6,12 @@ import (
 	"os"
 )
 
-func logHandler(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Println(r.Method, r.URL)
-		h.ServeHTTP(w, r)
-	})
-}
-
 func main() {
 	path, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	handler := logHandler(http.FileServer(http.Dir(path)))
-
 	log.Println("Serving on http://localhost:5000")
-	http.ListenAndServe(":5000", handler)
+	http.ListenAndServe(":5000", http.FileServer(http.Dir(path)))
 }
