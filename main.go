45eee2600eb2be4ad60d2196165e8a4d3a5f0f67
mihirsd <35537937+mihirsd@users.noreply.github.com>
2018-07-25 20:17:42 +0530

    Revert #4b60e09

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"os"
 )
 
+func logHandler(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL)
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 	path, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	handler := logHandler(http.FileServer(http.Dir(path)))
 
 	log.Println("Serving on http://localhost:5000")
-	http.ListenAndServe(":5000", http.FileServer(http.Dir(path)))
+	http.ListenAndServe(":5000", handler)
 }
